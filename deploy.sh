bcf1d465ecf61a58fa5b5c2f9b268414801d536a
Sebastien Saunier <seb@saunier.me>
2014-12-20 12:14:51 +0100

    No more FTP deploy

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd `dirname $BASH_SOURCE`
-HOST="ftp.lockyourscreen.com"
-USER=lockyour
-PASSWORD=`security find-generic-password -l "$HOST" -g 2>&1 | grep "password" | cut -d \\" -f 2`
-echo "cd www\nm\nmput *" | ftp -i ftp://$USER:$PASSWORD@$HOST
